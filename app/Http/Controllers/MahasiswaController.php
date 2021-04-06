928582425e4883c7241d816e889cd29d91f03fcf
ilhamibrahim-id <ii.ilhamibrahim@gmail.com>
2021-04-06 15:29:00 +0700

    edit function create agar dapat mengambil data kelas

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -28,7 +28,8 @@ class MahasiswaController extends Controller
      */
     public function create()
     {
-        return view('mahasiswas.create');
+        $kelas = Kelas::all();
+        return view('mahasiswas.create',['kelas' => $kelas]);
     }
 
     /**
